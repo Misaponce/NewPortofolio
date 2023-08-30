2e5de7c7f7717f4c831fc748973a2238af0a2698
Misaponce <poncemisael@gmail.com>
2023-08-30 12:32:54 +0100

    fixed issue with navbar not being sticky to the top

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,49 +1,17 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import '../stylesheet/Navbar.css';
 import { LanguageSwitcher } from './LanguageSwitcher';
 import { useTranslation } from 'react-i18next';
 import { ListAnimation } from './Animations/ListAnimation';
-import { color } from 'framer-motion';
 
 
 export const Navbar = () => {
     const { t } = useTranslation();
 
-    // Setting 'active' class
-    // This CustomClass will be trigger when scrolling and will add 'active' to enable bootstrap style for active elements
-    function CustomClass({ sectionId, children }) {
-        const [isActive, setIsActive] = useState(false);
-        const handleScroll = () => {
-          const section = document.getElementById(sectionId);
-          if (section) {
-            const rect = section.getBoundingClientRect();
-            // rect.top and rect.bottom represents the position of the section relative to the viewport
-            // window.innerHeight viewport height
-            // this if statement calculates half of the viewport to determine whether the section is visible or not
-            const isVisible = rect.top <= window.innerHeight * 0.5 && rect.bottom >= window.innerHeight * 0.5;
-            setIsActive(isVisible);
-          } 
-        };
-      
-        useEffect(() => {
-          window.addEventListener('scroll', handleScroll);
-          return () => {
-            window.removeEventListener('scroll', handleScroll);
-          };
-        }, []);
-
-        return (
-            // Return a link with an 'active' class with paremeter defined above
-            <a className={`nav-link ms-lg-5 ms-md-5 ms-0 ${isActive ? 'active' : ''}`} aria-current="page" href={`#${sectionId}`}>
-                {children}
-            </a>
-        )
-    }
-  
 
   return (
     <div>
-        <nav className="navbar bg-dark navbar-dark  navbar-expand-lg bg-body-tertiary fixed-top mb-5">
+        <nav className="navbar navbar-dark navbar-expand-lg bg-dark fixed-top">
             <div className="container-fluid">
                 <a className="navbar-brand ms-lg-2 px-lg-2" href="#">
                     <strong className='logo-letter'>M</strong>
@@ -54,7 +22,7 @@ export const Navbar = () => {
                 </button>
                 <div className="offcanvas offcanvas-end text-bg-dark" tabIndex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
                 <div className="offcanvas-header">
-                    <h5 className="offcanvas-title" id="offcanvasNavbarLabel">
+                    <h5 className="offcanvas-title" id="offcanvasDarkNavbarLabel">
                         <strong className='logo-letter'>M</strong>
                         <strong className='logo-dot'>.</strong>
                     </h5>
@@ -66,9 +34,15 @@ export const Navbar = () => {
                     ulClass={"navbar-nav justify-content-end flex-grow-1 pe-3"}
                     liClass={"nav-item"}
                 >
-                    <CustomClass sectionId='about'>{t('commonTranslations.navBar.about')}</CustomClass>
-                    <CustomClass sectionId='projects'>{t('commonTranslations.navBar.projects')}</CustomClass>
-                    <CustomClass sectionId='contact'>{t('commonTranslations.navBar.contact')}</CustomClass>
+                    <a className={`nav-link ms-lg-5 ms-md-5 ms-0`} aria-current="page" href='#about'>
+                        {t('commonTranslations.navBar.about')}
+                    </a>
+                    <a className={`nav-link ms-lg-5 ms-md-5 ms-0`} aria-current="page" href='#projects'>
+                        {t('commonTranslations.navBar.projects')}
+                    </a>
+                    <a className={`nav-link ms-lg-5 ms-md-5 ms-0`} aria-current="page" href='#contact'>
+                        {t('commonTranslations.navBar.contact')}
+                    </a>
                     {/* Language Switcher */}
                     <LanguageSwitcher/>
                 </ListAnimation>
